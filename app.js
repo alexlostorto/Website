e6e63013754ee1ab7a4a3166d874a180a024eaf1
Alex Lo Storto <lostorto.business@gmail.com>
2022-11-02 20:41:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const aboutCategory = document.querySelector('.about');
 const projectsCategory = document.querySelector('.projects');
 const aboutPage = document.querySelector('.about-page');
 const projectsPage = document.querySelector('.projects-page');
+const cursor = document.querySelector('.cursor');
 
-quotes = [
+const quotes = [
     'Enjoy the little things',
     'Sm;)e',
     'Hello there!',
@@ -32,9 +33,50 @@ function fadeIntro() {
     }, 200)
 }
 
+let quote = quotes[Math.floor(Math.random() * quotes.length)];
+let i = 0;
+let speed = 100;
+
+function typewrite() {
+
+    if (i < quote.length) {
+        quoteElement.innerHTML += quote.charAt(i);
+        i++;
+        setTimeout(typewrite, speed);
+    } else {
+        setTimeout(() => {
+            quoteElement.classList.add('fade');
+        }, 1000)
+    }
+}
+
+window.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+    cursor.setAttribute('data-from-top', (cursor.offsetTop - scrollY));
+})
+
+window.addEventListener('scroll', () => {
+    const fromTop = parseInt(cursor.getAttribute('data-from-top'));
+    cursor.style.top = scrollY + fromTop + 'px';
+})
+
 window.addEventListener('DOMContentLoaded', () => {
-    // quote = quotes[Math.floor(Math.random() * quotes.length)];
-    // quoteElement.textContent = quote;
+    setTimeout(typewrite, 2000)
+
+    // Check if mobile or desktop 
+    let isDesktop = true;
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) {
+        isDesktop = false;
+    }
+
+    let isMobile = window.matchMedia("(any-pointer:coarse)").matches;
+    if (!isMobile && isDesktop) {
+        console.log("Desktop");
+        cursor.style.display = 'block';
+    } else {
+        console.log("Mobile");
+    }
 
     setTimeout(() => {
         logoSpan.forEach((span, index) => {
@@ -50,12 +92,14 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
         aboutCategory.addEventListener('click', function() {
-            console.log("clicked")
+            console.log("clicked");
+            cursor.style.background = 'black';
             fadeIntro();
             aboutPage.classList.add('active');
         });
 
         projectsCategory.addEventListener('click', function() {
+            cursor.style.background = 'black';
             fadeIntro();
             projectsPage.classList.add('active');
         });
